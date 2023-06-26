88659f6d639eb71be9adc4ba37bead74af486e69
AbdelrahmanWM <abdelrahmanwaleed0000@gmail.com>
2023-06-26 07:24:09 -0400

    Still solving problems preventing the heroku deployment

--- src/app.js
+++ src/app.js
@@ -8,8 +8,8 @@ const app = express()
 const port = process.env.PORT || 3000
 // Define paths for express config
 const publicDirPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates\\views')
-const partialsPath = path.join(__dirname, '../templates\\partials')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
