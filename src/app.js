d3aa3ff5cbe71edc690b198ca7537fdd30f3c10b
AbdelrahmanWM <abdelrahmanwaleed0000@gmail.com>
2023-06-26 06:50:36 -0400

    some simple changes

--- src/app.js
+++ src/app.js
@@ -7,9 +7,9 @@ const forecast = require('./utils/forecast')
 const app = express()
 const port = process.env.PORT || 3000
 // Define paths for express config
-const publicDirPath = path.join(__dirname, '..', '\\public')
-const viewsPath = path.join(__dirname, '..', '\\templates\\views')
-const partialsPath = path.join(__dirname, '..', '\\templates\\partials')
+const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates\\views')
+const partialsPath = path.join(__dirname, '../templates\\partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
